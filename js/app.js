490df434f00aa1092b9c36f42d0b553e21794734
Blay199 <a.sorrentino03@gmail.com>
2022-07-21 17:53:11 +0200

    menu a tendina

--- js/app.js
+++ js/app.js
@@ -9,15 +9,11 @@ const buttonElement = document.querySelector('input[type="button"]');
 buttonElement.addEventListener('click', 
 
 function() {
-    const inputAge = document.querySelector ('input[name="age"]').value;
+    const inputName = document.querySelector ('input[name="name"]').value;
     const inputKm = document.querySelector ('input[name="km"]').value; 
     let trainTicket = inputKm * 0.21;
 
-    if (inputAge < 18) {
-        trainTicket = trainTicket - trainTicket * 0.2;
-    } else if (inputAge > 65) {
-        trainTicket = trainTicket - trainTicket * 0.4;
-    }
+    
     
     
     console.log(trainTicket)
