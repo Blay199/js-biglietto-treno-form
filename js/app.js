51d9c4aa5fa7959d9290ca41d25424e72c477974
Blay199 <a.sorrentino03@gmail.com>
2022-07-21 18:05:13 +0200

    Non funziona MAIIIII

--- js/app.js
+++ js/app.js
@@ -9,11 +9,15 @@ const buttonElement = document.querySelector('input[type="button"]');
 buttonElement.addEventListener('click', 
 
 function() {
-    const inputName = document.querySelector ('input[name="name"]').value;
+    const ticketType = document.querySelector ('input[name="age"]').value;
     const inputKm = document.querySelector ('input[name="km"]').value; 
     let trainTicket = inputKm * 0.21;
 
-    
+    if (ticketType = 'Minorenne') {
+        trainTicket = trainTicket - trainTicket * 0.20
+    } else if (ticketType = Over65) {
+        trainTicket = trainTicket - trainTicket * 0.40
+    }
     
     
     console.log(trainTicket)
