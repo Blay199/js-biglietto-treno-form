6560111ec13200200163985394836bc514728c7a
Blay199 <a.sorrentino03@gmail.com>
2022-07-21 17:37:10 +0200

    Ripusho

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ const buttonElement = document.querySelector('input[type="button"]');
 buttonElement.addEventListener('click', 
 
 function() {
-    const inputAge = document.querySelector ('input[name="age"]');
-    const inputKm = document.querySelector ('input[name="km"]'); 
+    const inputAge = document.querySelector ('input[name="age"]').value;
+    const inputKm = document.querySelector ('input[name="km"]').value; 
     let trainTicket = inputKm * 0.21;
 
     if (inputAge < 18) {
